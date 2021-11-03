7d497ab649be902ca1972126485e34eec12b2ac3
Kristian Jensen <krnj@itu.dk>
2021-11-03 15:02:55 +0100

    Anonymize

--- scripts/run.individual.sh
+++ scripts/run.individual.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-cd /home/kristian/machamp
-
-source ./mtp/bin/activate
-
 MODEL=$1
 PARAMETERS=$2
 ITER=$3
 
 echo "Training $MODEL on $PARAMETERS"
 python machamp/train.py --dataset_config configs/Skills/$PARAMETERS.json --parameters_config configs/Skills/$MODEL.$ITER.json --device 1 --name skill.$MODEL.$PARAMETERS.$ITER
-
-deactivate
