a53089b60e880d36d5a9a09348eefa51c1cd5ce0
Kristian Jensen <krnj@itu.dk>
2021-11-03 15:03:55 +0100

    New significance testing

--- scripts/significance.py
+++ scripts/significance.py
@@ -2,48 +2,55 @@ import json
 import numpy as np
 import os
 from deepsig import multi_aso
-
+import pandas as pd
 
 if __name__ == "__main__":
     N = 5  # Number of random seeds
-    M = 4  # Number of different models / algorithms
+    M = 8  # Number of different models / algorithms
 
-    sources = ["big", "house", "tech", "Combined"]
+    model2idx = {
+        "Single-BERT": 1,
+        "Single-SpanBERT": 2,
+        "Single-JobBERT": 3,
+        "Single-JobSpanBERT": 4,
+        "Multi-BERT": 5,
+        "Multi-SpanBERT": 6,
+        "Multi-JobBERT": 7,
+        "Multi-JobSpanBERT": 8,
+    }
 
-    for source in sources:
-        skill = {}
-        knowledge = {}
-        multi = {}
-        for file in os.listdir("metrics/"):
-            if file.startswith(source):
-                source, eval_, model, _ = file.split(".")
-                with open("metrics/" + file, "r") as f:
-                    results = json.load(f)
-                    if eval_ == "Skills":
-                        skill[str(model)] = np.array(
-                            results["3.1 Per-Level Evaluation (outer chunks):"]["FB1"]
-                        )
-                    if eval_ == "Knowledge":
-                        knowledge[str(model)] = np.array(
-                            results["3.2 Per-Level Global Evaluation (inner chunks):"][
-                                "FB1"
-                            ]
-                        )
-                    if eval_ == "Multi":
-                        multi[str(model)] = np.array(
-                            results["1. Strict, Combined Evaluation (official):"]["FB1"]
-                        )
+    dev = {}
+    test = {}
 
-        # eps_min_skill = multi_aso(skill, confidence_level=0.05, return_df=True, num_jobs=4, use_symmetry=True)
-        eps_min_knowledge = multi_aso(
-            knowledge,
-            confidence_level=0.05,
-            return_df=True,
-            num_jobs=4,
-            use_symmetry=True,
-        )
-        # eps_min_multi = multi_aso(multi, confidence_level=0.05, return_df=True, num_jobs=6, use_symmetry=True)
+    for file in os.listdir("metrics_final/"):
+        source, eval, model, split, _ = file.split(".")
+        with open("metrics_final/" + file, "r") as f:
+            results = json.load(f)
+            if split == "dev":
+                k = model2idx[str(eval + "-" + model)]
+                dev[k] = np.array(
+                    results["1. Strict, Combined Evaluation (official):"]["FB1"]
+                )
+            else:
+                k = model2idx[str(eval + "-" + model)]
+                test[k] = np.array(
+                    results["1. Strict, Combined Evaluation (official):"]["FB1"]
+                )
 
-        # print(f"{source}, skill:\\ {eps_min_skill}")
-        print(f"{source}, knowledge:\\ {eps_min_knowledge}")
-        # print(f"{source}, multi:\\ {eps_min_multi}")
+    eps_min_dev = multi_aso(
+        dict(sorted(dev.items())),
+        confidence_level=0.05,
+        return_df=True,
+        num_jobs=64,
+        use_symmetry=True,
+    )
+    eps_min_test = multi_aso(
+        dict(sorted(test.items())),
+        confidence_level=0.05,
+        return_df=True,
+        num_jobs=64,
+        use_symmetry=True,
+    )
+
+    print(f"development:\\ {pd.DataFrame.to_latex(eps_min_dev)}")
+    print(f"test:\\ {pd.DataFrame.to_latex(eps_min_test)}")
