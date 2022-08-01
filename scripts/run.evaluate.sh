c48fc8223ed17bd9ab25c3c9949f506f78a6ccc2
Mike <mikz@itu.dk>
2022-08-01 11:00:31 +0200

    switch gold/preds paths.

--- scripts/run.evaluate.sh
+++ scripts/run.evaluate.sh
@@ -11,6 +11,6 @@ do
     for site in big house tech
     do
         echo "Evaluating $MODEL $site on $method"
-        python3 scripts/evaluate.py $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out --gold_file=$GPATH/corpus_"$site"_test"$EXTRA".conll
+        python3 scripts/evaluate.py $GPATH/corpus_"$site"_test"$EXTRA".conll $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out
     done
 done
