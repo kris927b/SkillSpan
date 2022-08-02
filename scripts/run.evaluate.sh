f741229706580d4716f1e98e27e361c7927065dc
Mike <mikz@itu.dk>
2022-08-02 09:52:33 +0200

    Modify pahts of evaluate and predict scripts.

--- scripts/run.evaluate.sh
+++ scripts/run.evaluate.sh
@@ -4,13 +4,13 @@ MODEL=$1
 EXTRA=$2
 
 GPATH="data/Skills/"
-PPATH="logs/$MODEL"
+PPATH="data/$MODEL"
 
 for method in Skills Knowledge Multi
 do
-    for site in big house tech
+    for site in house tech
     do
         echo "Evaluating $MODEL $site on $method"
-        python3 scripts/evaluate.py $GPATH/corpus_"$site"_test"$EXTRA".conll $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out
+        python3 scripts/evaluate.py $GPATH/skillspan_"$site"_test"$EXTRA".conll $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out
     done
 done
