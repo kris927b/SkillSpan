d1aab5d1d6f77da12d83414ef4ffb082152eed28
Mike Zhang <mikz@itu.dk>
2022-08-13 16:02:23 +0200

    Remove unused split line.

--- scripts/run.predict.sh
+++ scripts/run.predict.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 MODEL=$1 # e.g., jobbert
 TYPE=$2 # Skills Knowledge Multi
 SET=$3 # dev test
@@ -8,7 +7,6 @@ SET=$3 # dev test
 for i in 1 2 3 4 5
 do
     mkdir -p data/$MODEL
-#    python3 machamp/predict.py logs/skill.$MODEL.${TYPE,,}.$i/*/model.tar.gz data/Skills/corpus_big_$SET.conll data/$MODEL/$TYPE.big.$SET.$i.out --dataset big
     python3 machamp/predict.py logs/skill.$MODEL.${TYPE,,}.$i/*/model.tar.gz data/Skills/corpus_house_$SET.conll data/$MODEL/$TYPE.house.$SET.$i.out --dataset house
     python3 machamp/predict.py logs/skill.$MODEL.${TYPE,,}.$i/*/model.tar.gz data/Skills/corpus_tech_$SET.conll data/$MODEL/$TYPE.tech.$SET.$i.out --dataset tech
 done
