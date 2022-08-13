1b45592c1c0e13bfb796790df13c4ef0f899d1e8
Mike Zhang <mikz@itu.dk>
2022-08-13 15:59:31 +0200

    Modify evaluation script for models.

--- scripts/run.evaluate.sh
+++ scripts/run.evaluate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-MODEL=$1
-EXTRA=$2
-
 GPATH="data/Skills/"
-PPATH="data/$MODEL"
 
-for method in skills knowledge multi
+for model in bert jobbert spanbert jobspanbert
 do
-    for site in house tech
-    do
-        echo "Evaluating $MODEL $site on $method"
-        python3 scripts/evaluate.py $GPATH/skillspan_"$site"_test"$EXTRA".conll $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out
-    done
+  PPATH="data/$model"
+  for method in skills knowledge multi
+  do
+      for site in house tech
+      do
+          echo "Evaluating $model $site on $method"
+          python3 scripts/evaluate.py $GPATH/skillspan_"$site"_test.conll $PPATH/$method.$site.test.1.out $PPATH/$method.$site.test.2.out $PPATH/$method.$site.test.3.out $PPATH/$method.$site.test.4.out $PPATH/$method.$site.test.5.out
+      done
+  done
 done
